b05c21a295122d164df08bf85a485128812baec5
Rishab Saraf <rissaraf@amazon.com>
2016-08-23 10:23:09 +0545

    Using urllib instead of socket to read web content

--- pythonWebBrowser.py
+++ pythonWebBrowser.py
@@ -1,14 +1,7 @@
-import socket
+import urllib
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('www.py4inf.com',80))
+socketfile = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
 
-mysock.send('GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n')
+for line in socketfile:
+  print (line.strip())
 
-while True:
-  data = mysock.recv(512)
-  if len(data) < 1 :
-    break
-  print data
-
-mysock.close()
