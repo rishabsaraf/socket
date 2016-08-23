39dc0c9a40d684cdfe955dd37dd4cffaf85a74e3
Rishab Saraf <rissaraf@amazon.com>
2016-08-23 13:16:51 +0545

    Iteratively crawling urls

--- webcrawler.py
+++ webcrawler.py
@@ -2,11 +2,16 @@ import urllib
 from bs4 import BeautifulSoup
 
 url = raw_input('Enter the url: ')
-socketfile = urllib.urlopen(url)
+count = int(raw_input('Enter the count: '))
+position = int(raw_input('Enter the position: '))
 
-dataWithHtml = socketfile.read()
-soup = BeautifulSoup(dataWithHtml,'html.parser')
-anchorTags = soup('a')
+for i in range(count):
+  socketfile = urllib.urlopen(url)
+  dataWithHtml = socketfile.read()
+  soup = BeautifulSoup(dataWithHtml,'html.parser')
+  anchorTags = soup('a')
+  url = anchorTags[position-1].get('href')
+  name = anchorTags[position-1].contents[0]
 
-for tag in anchorTags:
-  print(tag.get('href', None))
+print(name)
+
